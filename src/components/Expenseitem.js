379ab040f0eb76440e4860776fbeb1540b399be2
narendrasingh26 <narendrajadon261990@gmail.com>
2023-03-31 20:53:33 +0530

    passing data

---
+++ src/components/Expenseitem.js
@@ -0,0 +1,17 @@
+  import './ExpenseItem.css'; 
+
+function ExpenseItem(props) {
+  const locationOfItem='locationOfExpenditure';
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+        <div className="expense-item__price" >{locationOfItem}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
