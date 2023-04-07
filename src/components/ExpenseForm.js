68cccfba5a27e64b1e61aa1e09eccae9147cadfe
narendrasingh26 <narendrajadon261990@gmail.com>
2023-04-07 22:52:14 +0530

    task 14 final

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -43,7 +43,7 @@ const ExpenseForm = (props) => {
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2023-01-01" max="2023-12-31" value={enteredDate}  onChange={dateChangeHandler} />
+          <input type="date" min="2020-01-01" max="2023-12-31" value={enteredDate}  onChange={dateChangeHandler} />
         </div>
       </div>
       <div className="new-expense__actions">
